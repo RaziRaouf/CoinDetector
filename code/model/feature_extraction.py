32fec322b04830c0bdd859c1f02549bfd0563388
akramch <akram.chaabnia25@gmail.com>
2024-03-27 22:39:14 +0100

    canny edge detection

---
+++ code/model/feature_extraction.py
@@ -0,0 +1,10 @@
+# feature_extraction.py
+import cv2
+import numpy as np
+
+def apply_canny_edge_detection(image, threshold1=100, threshold2=200):
+    # Apply Canny edge detection
+    edges = cv2.Canny(image, threshold1, threshold2)
+    
+    return edges
+
