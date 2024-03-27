da60a9e83e38b4d71a82c19634d355ad7fc7800f
akramch <akram.chaabnia25@gmail.com>
2024-03-27 22:40:06 +0100

    hough circle detection

--- code/model/feature_extraction.py
+++ code/model/feature_extraction.py
@@ -22,3 +22,39 @@ def display_contours(grayscale_image, contours):
     
     return image_with_contours
 
+def apply_hough_circle_detection(image, contours, hierarchy, dp=1, minDist=20, param1=50, param2=30, minRadius=0, maxRadius=0):
+    # Initialize empty list to store detected circles
+    all_circles = []
+
+    # Iterate through contours and hierarchy
+    for i, contour in enumerate(contours):
+        # Access the hierarchy information for the current contour
+        current_hierarchy = hierarchy[0][i]
+
+        # Check if the contour is an external contour and not a child contour
+        if current_hierarchy[3] == -1:
+            # Compute bounding box around contour
+            x, y, w, h = cv2.boundingRect(contour)
+
+            # Extract ROI (region of interest) from grayscale image
+            roi = image[y:y+h, x:x+w]
+
+            # Apply Hough Circle Transform to detect circles within ROI
+            circles = cv2.HoughCircles(roi, cv2.HOUGH_GRADIENT, dp, minDist, param1=param1, param2=param2, minRadius=minRadius, maxRadius=maxRadius)
+
+            # If circles are found, append them to the list of detected circles
+            if circles is not None:
+                circles = np.uint16(np.around(circles))
+                for circle in circles[0]:
+                    # Calculate center coordinates relative to the original image
+                    center = (circle[0] + x, circle[1] + y)
+                    radius = circle[2]
+                    all_circles.append((center, radius))
+
+    # Draw the detected circles on the original image
+    image_with_circles = np.copy(image)
+    for circle in all_circles:
+        center, radius = circle
+        cv2.circle(image_with_circles, center, radius, (255, 255, 255), 2)
+
+    return image_with_circles
--- code/model/model.py
+++ code/model/model.py
@@ -3,18 +3,23 @@ import cv2
 import matplotlib.pyplot as plt
 from preprocess import *
 from segmentation import *
+from feature_extraction import *
 
 def main():
-    image_path = "F:\\France\\paris_cite\\S2\\image\\projet\\CoinDetector\\dataset\\images\\9.jpg"
+    image_path = "F:\\France\\paris_cite\\S2\\image\\projet\\CoinDetector\\dataset\\images\\40.jpg"
     original_image = cv2.imread(image_path)
     grayscale_image = convert_to_grayscale(image_path)
     blurred_image = apply_gaussian_blur(grayscale_image)
     segmented_image = apply_otsu_threshold(blurred_image)
     dilation_image = apply_dilation(segmented_image)
     erosion_image = apply_erosion(dilation_image)
+    cannied_image = apply_canny_edge_detection(erosion_image)
+    contours, hierarchy = find_contours(cannied_image)
+    image_with_contours = display_contours(grayscale_image, contours)
+    image_with_circles = apply_hough_circle_detection(cannied_image, contours, hierarchy)
 
     # Display all six images side by side
-    fig, axes = plt.subplots(2, 3, figsize=(18, 10))
+    fig, axes = plt.subplots(3, 3, figsize=(18, 10))
 
     # Plot the original image
     axes[0, 0].imshow(cv2.cvtColor(original_image, cv2.COLOR_BGR2RGB))
@@ -37,14 +42,19 @@ def main():
     axes[1, 0].axis('off')
 
     # Plot the dilation image
-    axes[1, 1].imshow(dilation_image, cmap='gray')
-    axes[1, 1].set_title('Dilated Image')
+    axes[1, 1].imshow(cannied_image, cmap='gray')
+    axes[1, 1].set_title('Canny Image')
     axes[1, 1].axis('off')
 
-    # Plot the erosion image
-    axes[1, 2].imshow(erosion_image, cmap='gray')
-    axes[1, 2].set_title('Eroded Image')
+    # contour image
+    axes[1, 2].imshow(image_with_contours, cmap='gray')
+    axes[1, 2].set_title('Contour Image')
     axes[1, 2].axis('off')
+
+    # Plot the circles image
+    axes[2, 0].imshow(image_with_circles, cmap='gray')  # Corrected: Use 'image_with_circles'
+    axes[2, 0].set_title('Circles Image')
+    axes[2, 0].axis('off')
 
     plt.tight_layout()
     plt.show()
