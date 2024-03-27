681293884e869a678d1e681a4d7bdda945701aae
akramch <akram.chaabnia25@gmail.com>
2024-03-27 22:39:47 +0100

    contour detection

--- code/model/feature_extraction.py
+++ code/model/feature_extraction.py
@@ -8,3 +8,17 @@ def apply_canny_edge_detection(image, threshold1=100, threshold2=200):
     
     return edges
 
+def find_contours(image):
+    # Find contours
+    contours, hierarchy = cv2.findContours(image, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    return contours, hierarchy
+
+
+def display_contours(grayscale_image, contours):
+    # Draw contours on the original grayscale image
+    image_with_contours = cv2.cvtColor(grayscale_image, cv2.COLOR_GRAY2BGR)
+    cv2.drawContours(image_with_contours, contours, -1, (0, 255, 0), 2)
+    
+    return image_with_contours
+
