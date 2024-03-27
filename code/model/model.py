7e44938828bc8f8ee6e6bdaf5f5cd362b844b066
akramch <akram.chaabnia25@gmail.com>
2024-03-27 17:35:21 +0100

    noise reducation filters( gaussian and median blur), histogram equalization

--- code/model/model.py
+++ code/model/model.py
@@ -1,18 +1,40 @@
 # model.py
 import cv2
-from preprocess import convert_to_grayscale
+import matplotlib.pyplot as plt
+from preprocess import *
 
 def main():
-    image_path = "F:\France\paris_cite\S2\image\projet\CoinDetector\dataset\images\.png"
+    image_path = "F:\France\paris_cite\S2\image\projet\CoinDetector\dataset\images\9.jpg"
+    original_image = cv2.imread(image_path)
     grayscale_image = convert_to_grayscale(image_path)
+    apply_histogram_equalization = apply_gaussian_blur(grayscale_image)
+    blurred_image = apply_median_blur(apply_histogram_equalization)
 
-    # Display the original and grayscale images
-    if grayscale_image is not None:
-        original_image = cv2.imread(image_path)
-        cv2.imshow("Original Image", original_image)
-        cv2.imshow("Grayscale Image", grayscale_image)
-        cv2.waitKey(0)
-        cv2.destroyAllWindows()
+
+    # Display all three images side by side
+    fig, axes = plt.subplots(1, 4, figsize=(15, 5))
+
+    # Plot the original image
+    axes[0].imshow(cv2.cvtColor(original_image, cv2.COLOR_BGR2RGB))
+    axes[0].set_title('Original Image')
+    axes[0].axis('off')
+
+    # Plot the grayscale image
+    axes[1].imshow(grayscale_image, cmap='gray')
+    axes[1].set_title('Grayscale Image')
+    axes[1].axis('off')
+
+    # Plot the blurred image
+    axes[2].imshow(apply_histogram_equalization, cmap='gray')
+    axes[2].set_title('histogram Equalized image')
+    axes[2].axis('off')
+
+    # Plot the blurred image
+    axes[3].imshow(blurred_image, cmap='gray')
+    axes[3].set_title('Blurred image')
+    axes[3].axis('off')
+
+    plt.show()
 
 if __name__ == "__main__":
     main()
--- code/model/preprocess.py
+++ code/model/preprocess.py
@@ -8,7 +8,22 @@ def convert_to_grayscale(image_path):
         print("Error: Unable to load image")
         return None
     
-    # Convert the image to grayscale
     grayscale_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     
     return grayscale_image
+
+def apply_gaussian_blur(image, kernel_size=(5, 5)):
+    blurred_image = cv2.GaussianBlur(image, kernel_size, 0)
+    
+    return blurred_image
+
+def apply_median_blur(image, kernel_size=5):
+    blurred_image = cv2.medianBlur(image, kernel_size)
+    
+    return blurred_image
+
+def apply_histogram_equalization(image):
+    equalized_image = cv2.equalizeHist(image)
+    
+    return equalized_image
+
