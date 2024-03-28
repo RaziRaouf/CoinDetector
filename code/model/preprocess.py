3a58c7ee01ee796bdfb9b3f66f56370a589fdc4e
akramch <akram.chaabnia25@gmail.com>
2024-03-29 00:32:30 +0100

    more preprocess methods

--- code/model/preprocess.py
+++ code/model/preprocess.py
@@ -1,5 +1,6 @@
 # preprocess.py
 import cv2
+import numpy as np
 
 def convert_to_grayscale(image_path):
     image = cv2.imread(image_path)
@@ -26,4 +27,22 @@ def apply_histogram_equalization(image):
     equalized_image = cv2.equalizeHist(image)
     
     return equalized_image
+
+def apply_adaptive_histogram_equalization(image, clip_limit=2.0, tile_grid_size=(8, 8)):    
+    # Create CLAHE object
+    clahe = cv2.createCLAHE(clipLimit=clip_limit, tileGridSize=tile_grid_size)
+    
+    # Apply CLAHE
+    equalized_image = clahe.apply(image)
+    
+    return equalized_image
+
+
+def apply_gamma_correction(image, gamma=1.0):
+    # Apply gamma correction
+    gamma_inv = 1.0 / gamma
+    table = np.array([((i / 255.0) ** gamma_inv) * 255 for i in np.arange(0, 256)]).astype("uint8")
+    corrected_image = cv2.LUT(image, table)
+    return corrected_image
+
 
