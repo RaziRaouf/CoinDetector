412cfe289d80b26bdf25b444651281b155403fc8
akramch <akram.chaabnia25@gmail.com>
2024-04-14 17:35:49 +0200

    evaluate dataset

--- code/evaluation/evaluation.py
+++ code/evaluation/evaluation.py
@@ -2,7 +2,8 @@ import json
 import cv2
 import numpy as np
 
-from code.model.model import model_test
+from code.dataset import load_annotations
+from code.model.model import *
 
 
 def calculate_iou(pred_box, gt_box):
@@ -75,6 +76,44 @@ def evaluate_image(image_path, annotation_path, threshold=0.5):
     print("Nb Annotated Coins:", len(ground_truths))
 
     return {"F1 Score": f1_score, "Mean Detection Error (MDE)": mde, "Nb Detected Coins": len(predictions), "Nb Annotated Coins": len(ground_truths)}
+
+def evaluate_dataset(model_test, dataset, threshold=0.5):
+    """
+    Evaluates the model performance on a given dataset.
+
+    Args:
+        model_test (function): Function that takes an image path and returns predictions.
+        dataset (CoinDataset): Instance of CoinDataset for the data to evaluate on.
+        threshold (float, optional): Threshold for IoU to consider a detection correct (default: 0.5).
+
+    Returns:
+        dict: Dictionary containing average F1 score, MDE, and number of detected/annotated coins.
+    """
+    total_f1 = 0
+    total_mde = 0
+    total_detected = 0
+    total_annotated = 0
+
+    for image_path, label_path in zip(dataset.image_paths, dataset.annotation_paths):
+        predictions, _ = model_test(image_path)  # Assuming model_test returns predictions and discards other outputs
+        ground_truths = load_annotations(label_path)
+
+        f1_score = calculate_f1_score(predictions, ground_truths, threshold)
+        mde = calculate_mde(predictions, ground_truths)
+        total_f1 += f1_score
+        total_mde += mde
+        total_detected += len(predictions)
+        total_annotated += len(ground_truths)
+
+    average_f1 = total_f1 / len(dataset)
+    average_mde = total_mde / len(dataset)
+    return {
+        "Average F1 Score": average_f1,
+        "Mean Detection Error (MDE)": average_mde,
+        "Nb Detected Coins": total_detected,
+        "Nb Annotated Coins": total_annotated,
+    }
+
 
 def main():
     image_path = "dataset\\images\\40.jpg"
