52577e1c1a3d80f1678ecfc3c2a85ac827906d02
akramch <akram.chaabnia25@gmail.com>
2024-05-29 23:43:45 +0200

    choisir quel set a evaluer dans le terminal

--- code/main.py
+++ code/main.py
@@ -1,37 +1,37 @@
 from code.dataset import CoinDataset
 from code.evaluation.evaluation import evaluate_dataset
-from code.model.model import model_test
 
 def main():
-  # Define paths to your image and annotation directories
-  image_dir = "dataset/images"
-  annotation_dir = "dataset/labels"
+    # Le chemin d'accès aux images et aux annotations
+    image_dir = "dataset/images"
+    annotation_dir = "dataset/labels"
 
-  # Create a CoinDataset instance
-  dataset = CoinDataset(image_dir, annotation_dir)
+    # Création de l'objet dataset
+    dataset = CoinDataset(image_dir, annotation_dir)
 
-# Access the validation set using indexing (assuming validation set is stored in `val_images` and `val_annotations`)
-  validation_images = dataset.val_images
-  validation_annotations = dataset.val_annotations
+    # Demander à l'utilisateur quel set évaluer
+    set_choice = ""
+    while set_choice not in ['train', 'val', 'test']:
+        set_choice = input("Choisissez le set à évaluer (train, val, test): ")
 
-# Evaluate the model on the validation set
-  evaluation_results = evaluate_dataset(validation_images, validation_annotations)
+    # Charger les images et les annotations en fonction du choix de l'utilisateur
+    if set_choice == 'train':
+        images = dataset.train_images
+        annotations = dataset.train_annotations
+    elif set_choice == 'val':
+        images = dataset.val_images
+        annotations = dataset.val_annotations
+    else:
+        images = dataset.test_images
+        annotations = dataset.test_annotations
 
-# Print the evaluation results
-  print("Validation Set Evaluation:")
-  for metric, value in evaluation_results.items():
-    print(f"\t{metric}: {value}")
+    # Évaluation des images
+    evaluation_results = evaluate_dataset(images, annotations)
 
-"""
-  # Print evaluation results
-  print("Evaluation Results:")
-  for key, value in evaluation_results.items():
-    print(f"{key}: {value}")
-
-"""
+    # Afficher les résultats de l'évaluation
+    print(f"{set_choice.capitalize()} Set Evaluation:")
+    for metric, value in evaluation_results.items():
+        print(f"\t{metric}: {value}")
 
 if __name__ == "__main__":
     main()
-
-
-
